1859bc282ac99631bf6737d9e917f04d0043f1d2
pierre delarocque <delarocque.pierre@gmail.com>
2024-05-23 12:00:44 +0200

    modif accueil

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -3,10 +3,22 @@ import Profil from "../image/pierre.png";
 function Acceuil() {
   return (
     <>
-      <div>
-        <p>Bonjour, </p>
+      <div className="accueil-container">
+        <p>
+          Bonjour, Je m'appelle Pierre Delarocque, développeur web et mobile
+          passionné basé à Tours. Fort de plusieurs années d'expérience, je
+          maîtrise des technologies telles que React, Node.js, et Javascript.
+          J'ai travaillé sur divers projets, allant d'applications météo à des
+          jeux vidéo en ligne, tout en utilisant des méthodes agiles comme
+          SCRUM. En plus de mes compétences techniques, je suis reconnu pour mon
+          dynamisme, ma créativité et ma capacité à travailler en équipe.
+          Titulaire d'un Master en Sciences de l'Éducation et d'une Licence en
+          Sociologie, je suis toujours en quête de nouveaux défis pour créer des
+          solutions innovantes et efficaces.{" "}
+        </p>
+
+        <img className="photo-container" src={Profil} alt="" />
       </div>
-      <img src={Profil} alt="" />
     </>
   );
 }
