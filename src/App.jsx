9572aaa24c48da8c74cd7e6f14bb0f01a7fd5eff
pierre delarocque <delarocque.pierre@gmail.com>
2024-05-23 17:41:46 +0200

    scss contact

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,15 @@ import "./App.scss";
 function App() {
   return (
     <>
+      <header className="header-container">
+        <h1 className="titre-h1">Mon Portefolio</h1>
+      </header>
       <nav>
         <NavLink to="/">Portfolio</NavLink>
         <NavLink to="/projet">Projets</NavLink>
         <NavLink to="/contact">Contact</NavLink>
       </nav>
-      <header className="header-container">
-        <h1 className="titre-h1">Mon Portefolio</h1>
-      </header>
+
       <main>
         <Outlet />
       </main>
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,19 +1,40 @@
+import PhoneInput from "react-phone-number-input";
+import { useState } from "react";
 import cv from "../image/cvdelarocque.jpg";
 
 function Contact() {
+  const [value, setValue] = useState();
   return (
     <>
-      <div>
-        <h2>Pierre Delarocque</h2>
-        <p>Tours</p>
-        <p>06.58.58.11.89</p>
-        <p>delarocque.pierre@gmail.com</p>
-        <p>https://github.com/PierreDelarocque</p>
-        <p>https://www.linkedin.com/in/pierre-delarocque-972740aa/</p>
+      <div className="contact-container">
+        <address>
+          <a href="tel:0658581189">06.58.58.11.89</a>
+
+          <a href="mailto:delarocque..pierre@gmail.com" target="_blank">
+            Email
+          </a>
+
+          <a href="https://github.com/PierreDelarocque" target="_blank">
+            {" "}
+            Github{" "}
+          </a>
+          {""}
+          <a
+            href="https://www.linkedin.com/in/pierre-delarocque-972740aa/"
+            target="_blank"
+          >
+            Linkedin
+          </a>
+        </address>
       </div>
       <div>
-        <h2>Mon CV</h2>
-        <img src={cv} alt="cv" />
+        <h2
+          className="title-h2
+      "
+        >
+          Mon CV
+        </h2>
+        <img className="cv-img" src={cv} alt="cv" />
       </div>
     </>
   );
